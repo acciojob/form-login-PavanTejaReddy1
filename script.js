269144839a6d4744ba39e47201df8f06cee9caf2
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-19 23:28:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
+const form = document.getElementById("form1");
 
+form.addEventListener("submit",getFormvalue);
+
+function getFormvalue(e) {
+	e.preventDefault();
+
+	const firstName = e.target.fname.value;
+	const lastName = e.target.lname.value;
+	
+	alert(`${firstName} ${lastName}`);
 }
